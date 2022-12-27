43a5afca05743e554ac9a385ab44806c039683c8
Arathi K <91886394+Arathi-K@users.noreply.github.com>
2022-12-27 12:54:20 +0530

    Add files via upload

---
+++ Assgn_1(form_using_html_php)(store_details_into_csv_file)/evaluation1/b200689cs_index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>B200689CS</title>
+</head>
+<body>
+  <h2 align=center>
+    <font color=red>Enter Student Details</font>
+</h2>
+  <form method="post">
+  <center>
+    <b>First Name:</b> &nbsp &nbsp<input type="text" name="fn"><br><br>
+    <b>Last Name:</b>&nbsp &nbsp<input type="text" name="ln"><br><br>
+    <b>Roll Number:</b><input type="text" name="roll"><br><br>
+    <b>Email:</b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp<input type="email" name="email"><br><br>
+    <input type="submit" name="insert" value=Insert>
+    <input type="submit" name="delete" value=Delete>
+    <input type="submit" name="search" value=Search>
+    <input type="submit" name="update" value=Update>
+    <input type="submit" name="display" value=Display>
+    <input type="submit" name="count" value="Count of Total number of students">
+    </center>
+  </form>
+</body>
+</html>
+
+
+<?php
+              
+if(isset($_POST['insert']))
+{
+$a=$_POST['fn'];
+$b=$_POST['ln'];
+$c=$_POST['roll'];
+$d=$_POST['email'];
+$pos=strlen($d)-11;
+if(substr_compare($d,"@nitc.ac.in",$pos)!=0)
+  echo"Enter a valid email id";
+else{
+$fp = fopen('student.csv', 'r');
+$fs = filesize('student.csv');
+$seperator=',';
+$flag=0;
+while($row=fgetcsv($fp,$fs,$seperator)){
+  if($row[2]==$c){
+    echo "<b><br><font color=red>A student with this roll number already exists.</font>";
+    fclose($fp);
+    $flag=1;
+    break;
+  }
+}
+if($flag==0)
+{
+    fclose($fp);
+    $addtofile="$a,$b,$c,$d\n";
+
+    $fp = fopen('student.csv', 'a');
+    
+    fwrite($fp, $addtofile);
+    fclose($fp);
+}
+}
+}
+
+if(isset($_POST['search']))
+{
+$r=$_POST['roll'];
+$fp = fopen('student.csv', 'r');
+$fs = filesize('student.csv');
+$seperator=',';
+$flag=0;
+while($row=fgetcsv($fp,$fs,$seperator)){
+  if($row[2]==$r){
+    echo "<b>Roll Number:</b>  &nbsp$row[2]<br>";
+    echo "<b>First Name: </b> &nbsp$row[0]<br>";
+    echo "<b>Last Name:</b> &nbsp $row[1]<br>";
+    echo "<b>Email:</b> &nbsp $row[3]<br>";
+    $flag=1;
+    break;
+  }
+
+}
+if($flag==0)
+{
+  echo "<b><font color=red>Student details not found!</font></b>";
+}
+fclose($fp);
+}
+if(isset($_POST['display']))
+{
+$fp = fopen('student.csv', 'r');
+$fs = filesize('student.csv');
+$seperator=',';
+$flag=0;
+while($row=fgetcsv($fp,$fs,$seperator)){
+  $flag=1;
+    echo "<hr><hr>";
+    echo "<b>FIRST NAME:</b> $row[0]<br>";
+    echo "<b>LAST NAME:</b> $row[1]<br>";
+    echo "<b>ROLL NUMBER:</b> $row[2]<br>";
+    echo "<b>EMAIL:</b> $row[3]";
+   
+}
+if($flag==0)
+  echo"<b><font color=red>No Details to display!!</font></b>";
+fclose($fp);
+}
+if(isset($_POST['count']))
+{
+$fp = fopen('student.csv', 'r');
+$fs = filesize('student.csv');
+$seperator=',';
+$count=0;
+while($row=fgetcsv($fp,$fs,$seperator))
+  $count++;
+echo"Number of students: $count";
+fclose($fp);
+}
+if(isset($_POST['update']))
+{
+  $a=$_POST['fn'];
+  $b=$_POST['ln'];
+  $c=$_POST['roll'];
+  $d=$_POST['email'];
+$fp = fopen('student.csv', 'r');
+$fp2= fopen('temp.csv', 'w');
+$fs = filesize('student.csv');
+$seperator=',';
+while($row=fgetcsv($fp,$fs,$seperator)){
+  if($row[2]==$c){
+
+    $addtofile="$a,$b,$c,$d\n";
+    fwrite($fp2, $addtofile);
+  }
+  else{
+    $addtofile="$row[0],$row[1],$row[2],$row[3]\n";
+    fwrite($fp2, $addtofile);
+  }
+}
+fclose($fp2);
+fclose($fp);
+unlink('student.csv');
+rename("temp.csv","student.csv");
+}
+if(isset($_POST['delete']))
+{
+  $c=$_POST['roll'];
+$fp = fopen('student.csv', 'r');
+$fp2= fopen('temp.csv', 'w');
+$fs = filesize('student.csv');
+$seperator=',';
+while($row=fgetcsv($fp,$fs,$seperator)){
+  if($row[2]!=$c){
+    $addtofile="$row[0],$row[1],$row[2],$row[3]\n";
+    fwrite($fp2, $addtofile);
+  }
+}
+fclose($fp2);
+fclose($fp);
+unlink('student.csv');
+rename("temp.csv","student.csv");
+}
+?>
